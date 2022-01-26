9fd524383004f97157bf89e4fc176d92b015a296
shannonb27 <shannon.bulian23@ycdsbk12.ca>
2022-01-26 16:56:23 -0500

    DONE ITS DONE TAKE IT TAKE IT NOW I'M DONE I DID IT

--- Sketch.java
+++ Sketch.java
@@ -7,7 +7,93 @@ import processing.core.PFont;
 import processing.core.PImage;
   
 public class Sketch extends PApplet {
-  PFont deathScreen;
+/** 
+ * A program Sketch.java that sends you into the game of Loyd Escape where you play as Loyd, and your goal is to escape the room you're put in by an old friend
+ * 
+* @author S. Bulian
+* @param steelOnDoor determines if the steel is on the wall
+* @param hasKey if character has the key
+* @param hasScrewdriver if Loyd has the screwdriver
+* @param firstText if the first text box is on the screen
+* @param upPressed if the up arrow is pressed
+* @param downPressed if the down arrow is pressed
+* @param leftPressed if the left arrow is pressed
+* @param rightPressed if the right arrow is pressed
+* @param simonSolved if the "simon says" puzzle is solved
+* @param simonScreen if the "simon says" puzzle is on the screen 
+* @param simonClick1 if the opening click of the "simon says" pizzle occurs
+* @param simonClick2 if the second click of the "simon says" pizzle occurs
+* @param simonClick3 if the third click of the "simon says" pizzle occurs
+* @param simonClick4 if the fourth click of the "simon says" pizzle occurs
+* @param simonStart if the "simon says" puzzle starts
+* @param simonDone if the "simon says" puzzle is complete
+* @param death if Loyd dies
+* @param openBookshelf if the bookshelf is on the screen
+* @param takeDonut if Loyd took a donut yet
+* @param villain1 if the first villain image is on the screen
+* @param villain2 if the second villain image is on the screen
+* @param box1 if the first text box is on the screen
+* @param box2 if the second text box is on the screen
+* @param box3 if the third text box is on the screen
+* @param box4 if the fourth text box is on the screen
+* @param box5 if the fifth text box is on the screen
+* @param box6 if the sixth text box is on the screen
+* @param box7 if the seventh text box is on the screen
+* @param box8 if the eighth text box is on the screen
+* @param box9 if the nineth text box is on the screen
+* @param screwDriverUp if you've picked up the screwdriver yet
+* @param realBook if the book from the bookshelf is opened
+* @param safeClick1 if the first click of the safe has occured
+* @param safeClick2 if the second click of the safe has occured
+* @param safeClick3 if the third click of the safe has occured
+* @param safeClick4 if the fourth click of the safe has occured
+* @param safeClick5 if the fifth click of the safe has occured
+* @param safeClick6 if the sixth click of the safe has occured
+* @param safeUnlocked if the safe is unlocked
+* @param safeOpen if the safe has been opened
+* @param openSafe to show the opened safe image
+* @param out if you've gotten out of the room/escaped
+* @param r variable used for a click of the red colour in simon says
+* @param g variable used for a click of the green colour in simon says
+* @param b variable used for a click of the blue colour in simon says
+* @param y variable used for a click of the yellow colour in simon says
+* @param simonReal[] the real "simon says" puzzle code
+* @param simonGuess[] the player's guess of the "simon says" puzzle
+* @param simonRectX[] simon says indicators as to where you are in the puzzle
+* @param safeLock[] the safe's true password
+* @param safeGuess[] your guess of the safe's password
+* @param loydX your character, loyd's x value
+* @param loydY your character, loyd's y value
+* @param villainX the villain's x value
+* @param villainY the villain's y value
+* @param bookX the closeup of the bookshelf's x value
+* @param bookY the closeup of the bookshelf's y value
+* @param map the image of the map/room you are in
+* @param loyd the image of your character, loyd
+* @param villainDonut the image of the villain with donuts
+* @param villain the image of the villain without donuts
+* @param villainDonutMinusOne the image of the villain without a single donut
+* @param closeBook the image of the closeup of the bookshelf
+* @param screwdriver the image of the screwdriver
+* @param filledBook the image of the book with the scribbles
+* @param safe the image of the locked safe
+* @param itOpen the image of the opened safe
+* @param text1 first text box, first line
+* @param text2 first text box, second line
+* @param text3 first text box, third line
+* @param text4 first text box, fourth line
+* @param text5 first text box, fifth line
+* @param textBox1 first text box as a whole
+* @param textTwo1 second text box text
+* @param textThree third text box text
+* @param textFour fourth text box text
+* @param textFive fifth text box text
+* @param textSix sixth text box text
+* @param textSeven seventh text box text
+* @param textEight eighth text box text
+* @param textNine nineth text box text
+ */
+//declare variables
   boolean steelOnDoor = false;
   boolean hasKey = false;
   boolean hasScrewdriver = false;
@@ -20,7 +106,6 @@ public class Sketch extends PApplet {
   boolean box2 = false;
   boolean simonSolved = false;
   boolean simonScreen = false;
-  boolean password = false;
   boolean simonClick1 = false;
   boolean simonClick2 = false;
   boolean simonClick3 = false;
@@ -29,7 +114,6 @@ public class Sketch extends PApplet {
   boolean simonDone = false;
   boolean death;
   boolean openBookshelf = false;
-  boolean mansThere;
   boolean takeDonut = false;
   boolean villain1 = false;
   boolean villain2 = false;
@@ -85,17 +169,17 @@ public class Sketch extends PApplet {
   String text5 = "Bucket's of fun rotting away loYd <3 (If you die in the game, quit and try again)";   
   String textBox1 = text1 + "\r\n" + text2 + "\r\n" + text3 +"\r\n" + text4  +text5;
   String textTwo1 = "Mysterious Figure: Loyddddddddddddd!! I've returned! Wait, how are you out of your chair? Oh no, did I do the thing that I sometimes do by capitalizing the letters that I am thinking about... DARN IT! Well, I brought us donuts, you can take one if you want..";
-  String textThree = "Mysterious Figure: You know Loyd, we haven't talked in a while, you may be surprised that I was the one who kidnapped you and attached you to an electric chair. Well, it all dates back to our college graduation, where you TRIPPED ME! You put a banana on the ground and I slipped and fell. I THOUGHT WE WERE FRIENDS LOYD! And you must be thinking, **is this vantri?** and the answer to that question.... is maybe........................ I THOUGHT WE WERE FRIENDS LOYD! *also smart one about getting out of the chair* Have fun trying to escape here for the rest of your life, with the donut being the only thing you'll eat until you collapse of hunger. byeeeeeeeee! (Click text box to continue)";
+  String textThree = "Mysterious Figure: You know Loyd, we haven't talked in a while, you may be surprised that I was the one who kidnapped you and attached you to an electric chair. Well, it all dates back to our college graduation, where you TRIPPED ME! You put a banana on the ground and I slipped and fell. I THOUGHT WE WERE FRIENDS LOYD! And you must be thinking, is this vantri? and the answer to that question.... is maybe........................ I THOUGHT WE WERE FRIENDS LOYD! also smart one about getting out of the chair... Have fun trying to escape here for the rest of your life, with the donut being the only thing you'll eat until you collapse of hunger. byeeeeeeeee! (Click text box to continue)";
   String textFour = "(You think to yourself) Of course it was Vantri. This guy never takes a joke... I mean maybe it was a little harsh, but he didn't break anything, and it's definitely not worth him covering my head with a bag and bringing me into a random place for me to DIE! Wait, I recognize this door.. is- is this Vantri's mom's house?? I KNOW WHERE I AM! MY HOUSE IS A FIVE MINUTE WALK FROM HERE! Ok, now I need to figure out how to get out of here. Is that a screwdriver on the floor? I thin, I'll need that, since the door has screws on the... VANTRI PUT STEEL ON THE DOOR???? Man, he's really salty about the banana thing, I kind of feel bad....... nah. It was funny. I guess I should pick up at screwdriver, and that bookshelf might have something I can use.";
   String textFive = "(You think to yourself) One of these books might help me, but which one....(Move your mouse all over all of the books to see which one you should look in)";
   String textSix = "(You think to yourself) I guess I should read this story, something seems off though. I guess these numbers are supposed to mean something? I don't know, what this substance is, but Vantri likes his chocolate milk, so I guess it's chocolate syrup... hopefully. (Click ALT to escape)";
   String textSeven = "(You think to yourself) Maybe I should try these numbers on that safe over there, hopefully a key for the locked door is there.";
   String textEight = "(You think to yourself) This must be it! I'll take this key, screw the screws off of the metal, and unlock the door! I'll be free!";
-  String textNine = "(You think to yourself) Alright, let's see if I can get out!";
+  String textNine = "(You think to yourself) Alright, let's see if I can get out! (Stand in front of the door and click it!)";
   public void settings() {
     
     size(600, 600);
-    
+    //load images
     map = loadImage("MAP.png");
     loyd = loadImage("Loyd.png");
     villain = loadImage("villain simple.png");
@@ -108,35 +192,23 @@ public class Sketch extends PApplet {
     itOpen = loadImage("openSafe.png");
     
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
-    String [] fontList = PFont.list();
-    printArray(fontList);
-    deathScreen = createFont("bazooka", 75);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-   System.out.println(mouseX + "," + mouseY + ", " + safeGuess[0] + safeGuess[1] + safeGuess[2] + safeGuess[3] + safeGuess[4] + safeGuess[5]);
+    //draw the inital appearance of the room
     image(map, 0, 0, width, height);
 	  electricChair();
     textBox();
 
-    
+    //establish collision
     if(loydY<70){
       loydY=70;
     }
     if(loydX>352 && loydY < 85){
       loydX = 352;
     }
-    
     if(loydY<88 && loydX <=514 && loydX >=412){
       loydY = 88;
     }
@@ -153,23 +225,25 @@ public class Sketch extends PApplet {
       wallSteel();
     }
     
-    
+    //if simon is solved, the villain walks in
     if(simonSolved == true){
       villain1 = true;
-        //villain with donut walks in
-  box2 = true;
-  if(villainX >= 115){
-    villainX = 115;
-    if(mouseX>145 && mouseX<244 && mouseY>221 && mouseX<244){
-    fill(211,245,211, 80);
-    rect(145, 221, 83, 23);
-    }
-    if(villainX <= -100){
-      villainX = -150;
+      box2 = true;
+
+       if(villainX >= 115){
+      villainX = 115;
+
+      if(mouseX>145 && mouseX<244 && mouseY>221 && mouseX<244){
+      fill(211,245,211, 80);
+      rect(145, 221, 83, 23);
+       }
+       if(villainX <= -100){
+         villainX = -150;
     }
       
     
   }
+  //villain walks in with donut
   if(villain1 && takeDonut == false){
   image(villainDonut, villainX, villainY);
     villainX++;
@@ -177,49 +251,54 @@ public class Sketch extends PApplet {
       noStroke();
       boxThing = true;
       }
-      
     }
     if(boxThing){
       fill(211,245,211, 80);
       rect(144, 222, 84, 23);
   }
+  //villain dissapears
   if(villain2){
     image(villainDonutMinusOne, villainX, villainY);
     box2 = false;
     box3 = true;
     villainX = -150;
     boxThing = false;
-
   }
-  
-      if (upPressed) {
+  //movement can now happen (if simon is solved)
+
+    if (upPressed) {
         loydY--;
         loydY--;
       }
-      if (downPressed) {
+    if (downPressed) {
         loydY++;
         loydY++;
       }
-      if (leftPressed) {
+    if (leftPressed) {
         loydX--;
         loydX--;
         
       }
-      if (rightPressed) {
+    if (rightPressed) {
         loydX++;
         loydX++;
     }
     }
-    image(loyd, loydX, loydY);
-    if(simonSolved == false){
+  //the creation of your character, Loyd
+  image(loyd, loydX, loydY);
+
+  //appearance of module on chair
+   if(simonSolved == false){
       simonChair();
     }
-    if(mouseX >= 247 && mouseX <= 300 && mouseY >= 300 && mouseY <= 320 && simonSolved == false){
+  
+  if(mouseX >= 247 && mouseX <= 300 && mouseY >= 300 && mouseY <= 320 && simonSolved == false){
       noStroke();
       fill(211,245,211, 90);
       rect(247, 300, 33, 20);
     }
-    if(simonScreen){
+  //simon says appearin on screen/the progress bar of simon
+  if(simonScreen){
       simonSays();
     }
    
@@ -244,6 +323,7 @@ public class Sketch extends PApplet {
 
   
 }
+//what happens when you die/get electrocuted
 if (death){
   fill(69, 3, 3, 160);
   noStroke();
@@ -256,17 +336,19 @@ if (death){
 }
 if(simonSolved == true){
 
-  //after villain donut, bookshelf
   if(loydX >=400 && loydY <= 88 && loydX<600 && mouseX >=400 && mouseY <88 && mouseX <600 && mouseY<88 && takeDonut == true){
     noStroke();
     fill(211,245,211, 90);
     rect(437, 31, 161, 149);
   }
 }
+//appearance of the bookshelf
 if(openBookshelf == true){
   image(closeBook, bookX, bookY);
   box5 = true;
   textBox();
+
+  //if your mouse is in the right spot, the real book shows itself
   if(mouseX > 182 && mouseY <329 && mouseX < 194 && mouseY >266){
     noStroke();
     fill(211,245,211, 90);
@@ -274,26 +356,31 @@ if(openBookshelf == true){
     realBook = true;
   }
 }
+
 if(screwDriverUp == false){
   image(screwdriver, 500, 400);
-  
 }
+
+//selection rectangle
 if(box4 == true && screwDriverUp == false && mouseX >= 518 && mouseY >= 417 && mouseX <= 550 && mouseY <= 520){
   noStroke();
   fill(211,245,211, 90);
   rect(518, 417, 59, 19);
 }
+
+//show image of the book
 if(realBook == true){
   image(filledBook, 92, 100);
   openBookshelf = false;
   box6 = true;
   textBox();
 }
+
+
 if(safeOpen == true){
   image(safe, 82, 90);
-  fill(255);
-  text(safeGuess[0] + safeGuess[1] + safeGuess[2] + safeGuess[3] + safeGuess[4] + safeGuess[5], 144,55);
 }
+
 if(openSafe == true){
   image(itOpen, 190, 196);
   safeOpen = false;
@@ -301,16 +388,20 @@ if(openSafe == true){
   hasKey = true;
   textBox();
 }
+
 if(box7 == true && loydX >0 && loydX <184 && loydY >234 && loydY <427 && mouseX > 0 && mouseX <95 && mouseY > 280 && mouseY <400){
   noStroke();
   fill(211,245,211, 90);
   rect(0, 280, 95, 120);
 }
+
 if(steelOnDoor == false && mouseX>= 59 && mouseY >=48 && mouseY<=147 && mouseX <= 150){
   noStroke();
   fill(211,245,211, 90);
   rect(59, 48, 100, 100);
 }
+
+//what happens when you leave the room after solving all of the puzzles
 if(out == true){
 fill(0);
 rect(0, 0, 600, 600);
@@ -321,11 +412,8 @@ text("(the end... for now)", 20, 500);
 }
 }
   
-    
- 
 
-
-  
+  //processing of the appearance of the electric chair
   public void electricChair(){
     fill(136,65,0);
     stroke(92,55,21);
@@ -351,6 +439,7 @@ text("(the end... for now)", 20, 500);
     ellipse(300, 250, 44, 15);
   }
 
+  //processing of the steel on the wall of the door
   public void wallSteel(){
     fill(200);
     stroke(150);
@@ -362,7 +451,7 @@ text("(the end... for now)", 20, 500);
     ellipse(65, 125, 4, 4);
     ellipse(135, 125, 4, 4);
   }
-
+//the text boxes appearing on screen
   public void textBox(){
     fill(0);
     stroke(255);
@@ -407,6 +496,7 @@ text("(the end... for now)", 20, 500);
     }
     }
   
+    //the simon says program appearance
   public void simonSays(){
     fill(180);
     stroke(160);
@@ -429,9 +519,10 @@ text("(the end... for now)", 20, 500);
     fill(43);
     rect(120, 524, 80, 25);
     fill(160);
-    
-    
+
   }
+
+  //the simon says on the chair's module
   public void simonChair(){
     rect(247, 300, 33, 20);
     noStroke();
@@ -450,6 +541,7 @@ text("(the end... for now)", 20, 500);
     line(250, 310, 278, 310);
   }
 
+  //certain things happening at different times when/where the mouse is clicked
   public void mouseClicked(){
     if(box7 == true && loydX >0 && loydX <184 && loydY >234 && loydY <427 && mouseX > 0 && mouseX <95 && mouseY > 280 && mouseY <400){
       safeOpen = true;
@@ -473,7 +565,7 @@ text("(the end... for now)", 20, 500);
   }
   
 }
-
+//simon says code
     if(mouseX >= 247 && mouseX <= 280 && mouseY >= 300 && mouseY <=320){
       simonScreen = true;
       simonStart = true;
@@ -571,10 +663,9 @@ text("(the end... for now)", 20, 500);
      }
      simonDone = true;
 }
-
+//safe code
 if(safeOpen == true){
   safeGuess[0] = 0;
- // safeClick1 = true;
 
 if(safeClick6 == true && mouseX >=243 && mouseX <= 270 && mouseY>= 247 && mouseY <=263){
 	safeUnlocked = true;
@@ -813,7 +904,7 @@ if(hasKey == true){
 
 
     
-
+//movement variables
   public void keyPressed() {
     if (keyCode == UP) {
       upPressed = true;
@@ -832,6 +923,7 @@ if(hasKey == true){
       box7 = true;
     }
   }
+  //removal of movement vairables
   public void keyReleased() {
     if (keyCode == UP) {
       upPressed = false;
